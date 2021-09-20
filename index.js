11b810ba3886f686b4c7c5a84ea45a0669b9ea28
Joshua Andes <andesjoshua@comcast.net>
2021-09-20 12:55:06 -0400

    commit tree 5880429c9c010fd1c7a55b33ed32da2b2e097a9e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 6;
+let num2 = 7;
+let num3 = 24;
+let num4 = 5;
+
+
+const multiply = num1 * num2 + 20;
+
+const random = Math.floor(Math.random() * 10 + 1)
+
+const mod = num3 % num4
+
+
+let max = Math.max(1,2,3,20)
